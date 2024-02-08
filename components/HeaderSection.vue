a5b82af70c1cbb6fdc25b647fc872a6bd1eccefe
Vladimir <daintymonk@gmail.com>
2024-02-08 22:43:16 +0300

    feat: add header component

---
+++ components/HeaderSection.vue
@@ -0,0 +1,113 @@
+<script lang="ts">
+	export default {
+		name: "HeaderSection",
+	};
+</script>
+
+<template>
+	<header class="header">
+		<NuxtLink to="/">
+			<img src="/header/logo.svg" alt="logo" width="100" height="60" />
+		</NuxtLink>
+		<nav>
+			<ul class="anchor-links" role="list">
+				<li class="anchor-links__item">
+					<NuxtLink class="link" :to="{ path: '/', hash: '#favorite' }"
+						>Favorite coffee</NuxtLink
+					>
+				</li>
+				<li class="anchor-links__item">
+					<NuxtLink class="link" :to="{ path: '/', hash: '#about' }"
+						>About</NuxtLink
+					>
+				</li>
+				<li class="anchor-links__item">
+					<NuxtLink class="link" :to="{ path: '/', hash: 'mobile' }"
+						>Mobile app</NuxtLink
+					>
+				</li>
+				<li class="anchor-links__item">
+					<NuxtLink class="link" :to="{ path: '/', hash: 'contact' }"
+						>Contact us</NuxtLink
+					>
+				</li>
+			</ul>
+		</nav>
+		<div class="menu-link">
+			<NuxtLink class="link" to="/menu">
+				Menu
+				<img
+					src="/icons/coffee-cup.svg"
+					alt="coffee-cup"
+					width="20"
+					height="20"
+				/>
+			</NuxtLink>
+		</div>
+	</header>
+</template>
+
+<style lang="scss" scoped>
+	.header {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
+		margin: 20px 0;
+	}
+
+	.anchor-links {
+		display: flex;
+		flex-wrap: wrap;
+		margin: 1rem 0 0 0;
+		padding: 0;
+
+		&__item {
+			margin-inline: 1.25rem;
+
+			&:last-child {
+				margin-right: 0;
+			}
+
+			&:first-child {
+				margin-left: 0;
+			}
+		}
+	}
+
+	.menu-link {
+		margin-top: 1rem;
+	}
+
+	.link {
+		position: relative;
+		font-size: var(--fs-300);
+		font-weight: 600;
+		line-height: 1.5;
+		color: var(--clr-700);
+		cursor: pointer;
+		text-decoration: none;
+
+		&:after {
+			content: "";
+			position: absolute;
+			left: 50%;
+			transform: translateX(-50%);
+			display: block;
+			width: 0;
+			height: 2px;
+			background-color: var(--clr-700);
+			transition: width 0.3s ease-in-out;
+		}
+
+		&:hover:after,
+		&:hover:focus {
+			width: 100%;
+		}
+
+		> img {
+			display: inline-block;
+			vertical-align: top;
+			margin-left: 5px;
+		}
+	}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,40 @@
+<script lang="ts">
+	import HeaderSection from "~/components/HeaderSection.vue";
+	export default {
+		components: { HeaderSection },
+	};
+</script>
+
 <template>
-	<Nuxt />
+	<div class="content-grid full-width">
+		<HeaderSection />
+		<Nuxt />
+	</div>
 </template>
+
+<style lang="scss" scoped>
+	.content-grid,
+	.full-width {
+		--padding-inline: 2.5rem;
+		--content-max-width: 85rem;
+
+		display: grid;
+		grid-template-columns:
+			[full-width-start] minmax(var(--padding-inline), 1fr)
+			[content-start] min(
+				100% - (var(--padding-inline) * 2),
+				var(--content-max-width)
+			)
+			[content-end]
+			minmax(var(--padding-inline), 1fr) [full-width-end];
+
+		> :not(.full-width),
+		.full-width > :not(.full-width) {
+			grid-column: content;
+		}
+
+		> .full-width {
+			grid-column: full-width;
+		}
+	}
+</style>
