21fc3873b2f219718b3fbe69955a3f075c0fc919
Vladimir <daintymonk@gmail.com>
2024-02-10 20:38:24 +0300

    feat: add button-icon-dark component

---
+++ components/v-btn-icon-dark.vue
@@ -0,0 +1,40 @@
+<script>
+	import VIcon from "./v-icon";
+	export default {
+		name: "VBtnIconDark",
+		components: { VIcon },
+	};
+</script>
+
+<template>
+	<button class="btn-icon_dark">
+		<VIcon width="24" height="24" stroke-color="none">
+			<slot />
+		</VIcon>
+	</button>
+</template>
+
+<style lang="scss" scoped>
+	.btn-icon_dark {
+		width: 3.75rem;
+		height: 3.75rem;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background-color: transparent;
+		border: 0.06rem solid var(--clr-600);
+		border-radius: 6.25rem;
+
+		&:hover {
+			background-color: var(--clr-600);
+		}
+
+		& > svg {
+			stroke: var(--clr-600);
+		}
+
+		&:hover > svg {
+			stroke: var(--clr-200);
+		}
+	}
+</style>
