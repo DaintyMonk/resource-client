f307c5ef014b67492f9610fa4eb773e611d0ec16
Vladimir <daintymonk@gmail.com>
2024-02-10 21:04:59 +0300

    feat: add button-store component

---
+++ components/v-btn-store.vue
@@ -0,0 +1,84 @@
+<script>
+	import VIcon from "./v-icon";
+	export default {
+		name: "VBtnStore",
+		components: { VIcon },
+		props: {
+			text: {
+				type: String,
+				default: () => "Available on",
+			},
+			storeName: {
+				type: String,
+				default: () => "",
+			},
+		},
+	};
+</script>
+
+<template>
+	<div class="btn-store">
+		<VIcon
+			class="btn-store__icon"
+			width="36"
+			height="36"
+			icon-color="none"
+			stroke-color="none"
+		>
+			<slot />
+		</VIcon>
+		<div class="btn-store__content">
+			<p class="btn-store__text main-text_caption">{{ text }}</p>
+			<span class="btn-store__name">{{ storeName }}</span>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	.btn-store {
+		display: inline-flex;
+		align-items: center;
+		width: 12.5rem;
+		height: 4rem;
+		margin-right: 1rem;
+		padding: 0.75rem 1.25rem;
+		border: 0.06rem solid var(--clr-600);
+		border-radius: 6.25rem;
+
+		&__icon {
+			fill: var(--clr-700);
+		}
+
+		&__content {
+			margin-left: 0.5rem;
+		}
+
+		&__text {
+			margin: 0 0 0.125rem 0;
+			color: var(--clr-700);
+		}
+
+		&__name {
+			font-size: var(--fs-300);
+			font-weight: var(--fw-bold);
+			line-height: 1.5;
+			color: var(--clr-700);
+		}
+
+		&:hover {
+			background-color: var(--clr-600);
+		}
+
+		&:hover > &__icon {
+			fill: var(--clr-200);
+		}
+
+		&:hover > &__content > &__text {
+			color: var(--clr-200);
+		}
+
+		&:hover > &__content > &__name {
+			color: var(--clr-200);
+		}
+	}
+</style>
