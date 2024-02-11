db7026be84b2fa571ff7e448118f68b427601e95
Vladimir <daintymonk@gmail.com>
2024-02-12 02:41:05 +0300

    feat: add button-secondary component

---
+++ components/v-btn-secondary.vue
@@ -0,0 +1,27 @@
+<script>
+	export default {
+		name: "VBtnSecondary",
+	};
+</script>
+
+<template>
+	<button class="btn_secondary btn-text">
+		<slot />
+	</button>
+</template>
+
+<style lang="scss" scoped>
+	.btn_secondary {
+		height: 2.75rem;
+		border: 0.0625rem solid var(--clr-700);
+		border-radius: 6.25rem;
+		background-color: transparent;
+		color: var(--clr-700);
+		cursor: pointer;
+
+		&:hover {
+			background-color: var(--clr-600);
+			color: var(--clr-200);
+		}
+	}
+</style>
