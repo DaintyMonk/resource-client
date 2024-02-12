f5df52f2f1cf9369a1632e9ff228e460acc6f2e1
Vladimir <daintymonk@gmail.com>
2024-02-12 18:11:04 +0300

    refactor: make the menu a separate component

---
+++ components/v-desktop-menu.vue
@@ -0,0 +1,110 @@
+<script>
+	import VIcon from "~/components/v-icon.vue";
+	import VCoffeeCupIcon from "~/components/icons/v-coffee-cup-icon.vue";
+	export default {
+		name: "VDesktopMenu",
+		components: { VCoffeeCupIcon, VIcon },
+		props: {
+			anchorLinks: {
+				type: Array,
+				default: () => [],
+			},
+		},
+	};
+</script>
+
+<template>
+	<div class="menu">
+		<NuxtLink to="/">
+			<img src="/header/logo.svg" alt="logo" width="100" height="60" />
+		</NuxtLink>
+		<nav class="menu__nav">
+			<ul class="menu__nav-links" role="list">
+				<li v-for="link in anchorLinks" :key="link.hash" class="menu__nav-item">
+					<NuxtLink
+						class="menu__nav-link link-text"
+						:to="{ path: link.path, hash: link.hash }"
+						>{{ link.text }}</NuxtLink
+					>
+				</li>
+			</ul>
+		</nav>
+		<div class="menu__nav">
+			<NuxtLink
+				class="menu__nav-link link-text"
+				to="/menu"
+				active-class="link_active"
+			>
+				Menu
+				<VIcon width="20" height="20" icon-color="none" stroke-color="none">
+					<VCoffeeCupIcon />
+				</VIcon>
+			</NuxtLink>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+	.menu {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
+
+		&__nav {
+			margin-top: 1rem;
+
+			&-links {
+				display: flex;
+				flex-wrap: wrap;
+				margin: 0;
+				padding: 0;
+			}
+
+			&-item {
+				margin-inline: 1.25rem;
+
+				&:last-child {
+					margin-right: 0;
+				}
+
+				&:first-child {
+					margin-left: 0;
+				}
+			}
+
+			&-link {
+				position: relative;
+				color: var(--clr-700);
+
+				> svg {
+					fill: transparent;
+					stroke: var(--clr-700);
+				}
+
+				&:after {
+					content: "";
+					position: absolute;
+					left: 50%;
+					transform: translateX(-50%);
+					display: block;
+					width: 0;
+					height: 0.125rem;
+					background-color: var(--clr-700);
+					transition: width 0.3s ease-in-out;
+				}
+
+				&:hover:after,
+				&:hover:focus,
+				&_active:after {
+					width: 100%;
+				}
+
+				> svg {
+					display: inline-block;
+					vertical-align: top;
+					margin-left: 0.3125rem;
+				}
+			}
+		}
+	}
+</style>
--- components/v-section-header.vue
+++ components/v-section-header.vue
@@ -1,119 +1,53 @@
 <script lang="ts">
-	import VIcon from "~/components/v-icon.vue";
-	import VCoffeeCupIcon from "~/components/icons/v-coffee-cup-icon.vue";
+	import VDesktopMenu from "~/components/v-desktop-menu.vue";
 	export default {
 		name: "VSectionHeader",
-		components: { VCoffeeCupIcon, VIcon },
+		components: { VDesktopMenu },
+		data() {
+			return {
+				anchorLinks: [
+					{
+						path: "/",
+						hash: "favorite",
+						text: "Favorite Coffee",
+					},
+					{
+						path: "/",
+						hash: "about",
+						text: "About",
+					},
+					{
+						path: "/",
+						hash: "mobile",
+						text: "Mobile App",
+					},
+					{
+						path: "/",
+						hash: "contact",
+						text: "Contact us",
+					},
+				],
+			};
+		},
 	};
 </script>
 
 <template>
 	<header class="header">
-		<NuxtLink to="/">
-			<img src="/header/logo.svg" alt="logo" width="100" height="60" />
-		</NuxtLink>
-		<nav>
-			<ul class="anchor-links" role="list">
-				<li class="anchor-links__item">
-					<NuxtLink class="link" :to="{ path: '/', hash: 'favorite' }"
-						>Favorite coffee</NuxtLink
-					>
-				</li>
-				<li class="anchor-links__item">
-					<NuxtLink class="link" :to="{ path: '/', hash: 'about' }"
-						>About</NuxtLink
-					>
-				</li>
-				<li class="anchor-links__item">
-					<NuxtLink class="link" :to="{ path: '/', hash: 'mobile' }"
-						>Mobile app</NuxtLink
-					>
-				</li>
-				<li class="anchor-links__item">
-					<NuxtLink class="link" :to="{ path: '/', hash: 'contact' }"
-						>Contact us</NuxtLink
-					>
-				</li>
-			</ul>
-		</nav>
-		<div class="menu-link">
-			<NuxtLink class="link" to="/menu" active-class="link_active">
-				Menu
-				<VIcon width="20" height="20" icon-color="none" stroke-color="none">
-					<VCoffeeCupIcon />
-				</VIcon>
-			</NuxtLink>
-		</div>
+		<VDesktopMenu class="header__menu" :anchor-links="anchorLinks" />
 	</header>
 </template>
 
 <style lang="scss" scoped>
 	.header {
-		display: flex;
-		flex-wrap: wrap;
-		justify-content: space-between;
 		margin: 1.25rem 0;
 	}
 
-	.anchor-links {
-		display: flex;
-		flex-wrap: wrap;
-		margin: 1rem 0 0 0;
-		padding: 0;
-
-		&__item {
-			margin-inline: 1.25rem;
-
-			&:last-child {
-				margin-right: 0;
+	@media (width < 1440px) {
+		.header {
+			&__menu {
+				display: none;
 			}
-
-			&:first-child {
-				margin-left: 0;
-			}
-		}
-	}
-
-	.menu-link {
-		margin-top: 1rem;
-	}
-
-	.link {
-		position: relative;
-		font-size: var(--fs-300);
-		font-weight: 600;
-		line-height: 1.5;
-		color: var(--clr-700);
-		cursor: pointer;
-		text-decoration: none;
-
-		> svg {
-			fill: transparent;
-			stroke: var(--clr-700);
-		}
-
-		&:after {
-			content: "";
-			position: absolute;
-			left: 50%;
-			transform: translateX(-50%);
-			display: block;
-			width: 0;
-			height: 0.125rem;
-			background-color: var(--clr-700);
-			transition: width 0.3s ease-in-out;
-		}
-
-		&:hover:after,
-		&:hover:focus,
-		&_active:after {
-			width: 100%;
-		}
-
-		> svg {
-			display: inline-block;
-			vertical-align: top;
-			margin-left: 0.3125rem;
 		}
 	}
 </style>
